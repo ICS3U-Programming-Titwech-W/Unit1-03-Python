93a2c5be95558ac71295529fdd45fd1a2ab7f988
Ubuntu <ubuntu@ip-172-31-78-88.ec2.internal>
2022-02-24 14:45:03 +0000

    hello world in style

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created By: Titwech
+# Date: feb 23,2022
+# says hello world in style
+
+
+def main():
+    print("Hello, World! With Style")
+
+
+if __name__ == "__main__":
+    main()
